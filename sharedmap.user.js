d79e1d69e33f9a1567faa7265dd50c99c89b36a7
xiaoyu <7547189+xytoki@users.noreply.github.com>
2022-12-17 23:18:24 +0800

    🐳 chore: update jsver

--- sharedmap.user.js
+++ sharedmap.user.js
@@ -13,7 +13,7 @@
 // @match       https://yuanshen.site/*.dll*
 // @match       https://static-web.ghzs.com/cspage_pro/yuanshenMap*
 // @grant       unsafeWindow
-// @version     1.3.0.2
+// @version     1.3.0.3
 // @author      YuehaiTeam
 // @description 让你的原神地图能定位，可共享(支持米游社大地图、空荧酒馆、光环助手)
 // @description:zh-CN 让你的原神地图能定位，可共享(支持米游社大地图、空荧酒馆、光环助手)
